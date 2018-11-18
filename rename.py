ab18f60e2d012c757b156f1aeb4ee4c903f01022
gustav-gustav <gustavo.guga.barros@hotmail.com>
2018-11-18 12:53:35 -0300

    added a new function that gets the episodes name

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-import os, glob
+import os, glob, re
 
 
 class Rename:
@@ -6,6 +6,7 @@ class Rename:
         self.main_path = path
         self.extension = extension
         self.files = glob.glob(os.path.join(self.main_path, f'*.{self.extension}'))
+        # print(self.files)
         self.expression = expression
         # self.rename()
         # self.replace_white_to_underline()
@@ -22,18 +23,32 @@ class Rename:
             except Exception as e:
                 print(str(e))
 
-    def rename(self):
+    def rename(self, optional=''):
         for i, f in enumerate(self.files):
             f_list = f.split("\\")
             name = f_list[-1]
             _, ext = os.path.splitext(name)
-            new_name = f'{self.expression}{str(i+1).zfill(2)}{ext}'
+            new_name = f'{self.expression}{str(i+1).zfill(2)}{optional[i]}{ext}'
             new_path = os.path.join(self.main_path, new_name)
-            try:
-                os.rename(f, new_path)
-            except Exception as e:
-                print(str(e))
+            # try:
+            #     os.rename(f, new_path)
+            # except Exception as e:
+            #     print(str(e))
+
+    def strip_ep_name(self):
+        optional = []
+        for i, f in enumerate(self.files):
+            f_list = f.split("\\")
+            name, ext = os.path.splitext(f_list[-1])
+            ep_name = name.strip((r'"Mr.Robot."(.*?)".720p.WEB-DL.x264-[MULVAcoded]"'))
+            ep_name = re.sub(r'^r.', '', ep_name)
+            ep_name = re.sub(r"eps2.\d{1,2}(.|_)", "", ep_name)
+            episode = ep_name.split('.')[1]
+            optional.append(f".{episode}")
+        self.rename(optional)
+
 
 
 if __name__ == "__main__":
-    Rename(input('Path: \n'), input('File extension: \n'), input('Expression to rename batch to: \n'))
+    # Rename(input('Path: \n'), input('File extension: \n'), input('Expression to rename batch to: \n'))
+    Rename("C:\\Users\\Gustavo\\Videos\\Mr.Robot.Season.2.720p", 'mkv', 'Mr.Robot.S02E').strip_ep_name()
