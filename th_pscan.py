a1bf2efa05f746b53cc7239183180458a74c6808
gustav <gustavo.guga.barros@hotmail.com>
2020-04-18 10:13:00 -0300

    modified th_pscan

--- th_pscan.py
+++ th_pscan.py
@@ -9,11 +9,6 @@ import socket
 # this is used so while one thread is using a variable, others cannot access
 # it. Once done, the thread releases the print_lock.
 # to use it, you want to specify a print_lock per thing you wish to print_lock.
-print_lock = threading.Lock()
-
-target = 'localhost'
-# target = 'https: // pythonprogramming.net/'
-
 ports=[]
 def portscan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,9 +21,8 @@ def portscan(port):
     except:
         pass
 
-
-# The threader thread pulls an worker from the queue and processes it
 def threader():
+    '''Pulls an worker from the queue and processes it'''
     while True:
         # gets an worker from the queue
         worker = q.get()
@@ -40,27 +34,23 @@ def threader():
         q.task_done()
 
 
-# Create the queue and threader
-q = Queue()
+if __name__ == '__main__':
+    print_lock = threading.Lock()
+    target = 'localhost'
+    # Create the queue and threader
+    q = Queue()
+    # how many threads are we going to allow for
+    for x in range(400):
+        t = threading.Thread(target=threader)
+        # classifying as a daemon, so they will die when the main dies
+        t.daemon = True
+        # begins, must come after daemon definition
+        t.start()
 
-# how many threads are we going to allow for
-for x in range(400):
-     t = threading.Thread(target=threader)
-
-     # classifying as a daemon, so they will die when the main dies
-     t.daemon = True
-
-     # begins, must come after daemon definition
-     t.start()
-
-
-start = time.time()
-
-# 100 jobs assigned.
-for worker in range(1, 8001):
-    q.put(worker)
-
-# wait until the thread terminates.
-q.join()
-
-print(f'Ports open: {ports}')
+    start = time.time()
+    # 100 jobs assigned.
+    for worker in range(1, 8001):
+        q.put(worker)
+    # wait until the thread terminates.
+    q.join()
+    print(f'Ports open: {ports}')
