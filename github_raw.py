55b028786d40a452a11411d18d25f1bca5c460b3
gustav <gustavo.guga.barros@hotmail.com>
2020-04-18 01:33:29 -0300

    added error handling

--- github_raw.py
+++ github_raw.py
@@ -1,17 +1,27 @@
 import requests, os, argparse
-
+from decorators import ResponseTimer
+requests.get = ResponseTimer(requests.get)
 
 def github_raw(module: str, repository: str, user: str='gustav-gustav', branch: str='master', path: str=os.getcwd()):
     '''Downloads modules from github raw'''
     base_url = f'https://raw.githubusercontent.com/'
     filename = f'{module}.py'
+    full_path = os.path.join(path, filename)
     url = f'{base_url}/{user}/{repository}/{branch}/{filename}'
-    with requests.get(url) as response:
-        if response.ok:
-            with open(filename, 'wb') as raw:
-                raw.write(response.content)
+    try:
+        with requests.get(url) as response:
+            if response.ok:
+                with open(full_path, 'wb') as raw:
+                    print(f'Creating {full_path}')
+                    raw.write(response.content)
+            else:
+                response.raise_for_status()
+
+    except requests.exceptions.HTTPError as httperr:
+        if httperr.response.status_code == 404:
+            print(f'No module named {module} in {repository}')
         else:
-            response.raise_for_status()
+            raise httperr
 
 
 if __name__ == '__main__':
@@ -22,4 +32,4 @@ if __name__ == '__main__':
     parser.add_argument('--branch', '-b', dest='branch', action='store', type=str, default='master')
     parser.add_argument('--path', '-p', dest='path', action='store', type=str, default=os.getcwd())
     args = parser.parse_args()
-    github_raw(module=args.module, repository=args.repository, user=args.user, branch=args.branch)
+    github_raw(**vars(args))
